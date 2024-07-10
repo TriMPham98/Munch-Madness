711665809a53822cf3596d95d3900e125e9404ba
Tri <trimpham98@gmail.com>
2024-07-09 23:18:09 -0700

    Add TODO comment

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // TODO: Display remaining choices in a bracket instead of a list
 // TODO: In a group of people choosing, add animation using Three.js 
 // TODO: where you have to flick a ball in to cast your vote
+// TODO: Include a draft where you select your favorite restaurants to enter into the bracket
 
 const inputSection = document.getElementById("inputSection");
 const bracketSection = document.getElementById("bracketSection");
