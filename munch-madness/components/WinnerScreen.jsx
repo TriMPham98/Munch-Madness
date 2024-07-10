f1ae88f547869f49a9ee2bf627ea3dbb1314aa9c
Tri <trimpham98@gmail.com>
2024-07-10 12:47:33 -0700

    Initial commit

--- munch-madness/components/WinnerScreen.jsx
+++ munch-madness/components/WinnerScreen.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "../components/ui/card";
-import { Button } from "../components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Button } from "./ui/button";
 import { Trophy } from "lucide-react";
 
 const WinnerScreen = ({ winner, onRestart }) => {
