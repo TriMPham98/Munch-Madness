7da5476eebf61e7a34c1adb775014d7ccb517f20
Tri <trimpham98@gmail.com>
2026-05-08 05:58:37 -0700

    Delete all files

--- munch-madness/components/ui/button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const Button = ({ className, ...props }) => (
-  <button
-    className={`inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-primary text-primary-foreground hover:bg-primary/90 h-10 py-2 px-4 ${className}`}
-    {...props}
-  />
-);
